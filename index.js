6a7696ab67c3c1a2199b2853e2cc7ed6e18c02ab
Aicy-nina <muhikanina@gmail.com>
2024-03-26 16:20:32 +0300

    Review arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+
+
+let random = Math.floor(Math.random()*100)+1;
+
+
+let num3 = 14; //Change num3 value to 14
+let num4 = 5; //Change num4 value to 5
+let mod = num3 % num4;
+
+
+let max =Math.max(20);
+Math.max(99,57,20,33,1);
+console.log(max)
